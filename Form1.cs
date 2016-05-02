10a3b4987fee9f327e4767d305c5f2c80ee80b1a
imNP <tibia615@yahoo.com>
2016-05-02 10:16:09 -0500

    Initial Commit

--- Form1.cs
+++ Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace InventoryDB
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        public frmMain()
         {
             InitializeComponent();
         }
