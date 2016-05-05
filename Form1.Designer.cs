22b56c724335e33e33653f94afc58ad0b59a1d87
imNP <tibia615@yahoo.com>
2016-05-05 10:50:27 -0500

    Added Connection String

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,271 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtItemNum = new System.Windows.Forms.TextBox();
+            this.txtItemDesc = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUnitAmt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUnitPrice = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtRetailPrice = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtProfitMargin = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.inventoryDataSet = new InventoryDB.InventoryDataSet();
+            this.inventoryTableBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.inventoryTableTableAdapter = new InventoryDB.InventoryDataSetTableAdapters.InventoryTableTableAdapter();
+            this.tableAdapterManager = new InventoryDB.InventoryDataSetTableAdapters.TableAdapterManager();
+            this.inventoryTableDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.inventoryDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inventoryTableBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inventoryTableDataGridView)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "itemNum: ";
+            // 
+            // txtItemNum
+            // 
+            this.txtItemNum.Location = new System.Drawing.Point(86, 32);
+            this.txtItemNum.Name = "txtItemNum";
+            this.txtItemNum.Size = new System.Drawing.Size(100, 20);
+            this.txtItemNum.TabIndex = 1;
+            // 
+            // txtItemDesc
+            // 
+            this.txtItemDesc.Location = new System.Drawing.Point(269, 32);
+            this.txtItemDesc.Name = "txtItemDesc";
+            this.txtItemDesc.Size = new System.Drawing.Size(100, 20);
+            this.txtItemDesc.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(209, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "itemDesc: ";
+            // 
+            // txtUnitAmt
+            // 
+            this.txtUnitAmt.Location = new System.Drawing.Point(449, 32);
+            this.txtUnitAmt.Name = "txtUnitAmt";
+            this.txtUnitAmt.Size = new System.Drawing.Size(100, 20);
+            this.txtUnitAmt.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(389, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "unitAmt: ";
+            // 
+            // txtUnitPrice
+            // 
+            this.txtUnitPrice.Location = new System.Drawing.Point(632, 32);
+            this.txtUnitPrice.Name = "txtUnitPrice";
+            this.txtUnitPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtUnitPrice.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(572, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "unitPrice: ";
+            // 
+            // txtRetailPrice
+            // 
+            this.txtRetailPrice.Location = new System.Drawing.Point(823, 32);
+            this.txtRetailPrice.Name = "txtRetailPrice";
+            this.txtRetailPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtRetailPrice.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(763, 35);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "retailPrice: ";
+            // 
+            // txtProfitMargin
+            // 
+            this.txtProfitMargin.Location = new System.Drawing.Point(1022, 32);
+            this.txtProfitMargin.Name = "txtProfitMargin";
+            this.txtProfitMargin.Size = new System.Drawing.Size(100, 20);
+            this.txtProfitMargin.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(944, 35);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Profit Margin: ";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(575, 104);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 12;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // inventoryDataSet
+            // 
+            this.inventoryDataSet.DataSetName = "InventoryDataSet";
+            this.inventoryDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // inventoryTableBindingSource
+            // 
+            this.inventoryTableBindingSource.DataMember = "InventoryTable";
+            this.inventoryTableBindingSource.DataSource = this.inventoryDataSet;
+            // 
+            // inventoryTableTableAdapter
+            // 
+            this.inventoryTableTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.InventoryTableTableAdapter = this.inventoryTableTableAdapter;
+            this.tableAdapterManager.UpdateOrder = InventoryDB.InventoryDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // inventoryTableDataGridView
+            // 
+            this.inventoryTableDataGridView.AutoGenerateColumns = false;
+            this.inventoryTableDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.inventoryTableDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4,
+            this.dataGridViewTextBoxColumn5,
+            this.dataGridViewTextBoxColumn6});
+            this.inventoryTableDataGridView.DataSource = this.inventoryTableBindingSource;
+            this.inventoryTableDataGridView.Location = new System.Drawing.Point(269, 143);
+            this.inventoryTableDataGridView.Name = "inventoryTableDataGridView";
+            this.inventoryTableDataGridView.Size = new System.Drawing.Size(644, 242);
+            this.inventoryTableDataGridView.TabIndex = 14;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "ItemNum";
+            this.dataGridViewTextBoxColumn1.HeaderText = "ItemNum";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "ItemDesc";
+            this.dataGridViewTextBoxColumn2.HeaderText = "ItemDesc";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "UnitAmt";
+            this.dataGridViewTextBoxColumn3.HeaderText = "UnitAmt";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.DataPropertyName = "UnitPrice";
+            this.dataGridViewTextBoxColumn4.HeaderText = "UnitPrice";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            // 
+            // dataGridViewTextBoxColumn5
+            // 
+            this.dataGridViewTextBoxColumn5.DataPropertyName = "RetailPrice";
+            this.dataGridViewTextBoxColumn5.HeaderText = "RetailPrice";
+            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            // 
+            // dataGridViewTextBoxColumn6
+            // 
+            this.dataGridViewTextBoxColumn6.DataPropertyName = "ProfitMargin";
+            this.dataGridViewTextBoxColumn6.HeaderText = "ProfitMargin";
+            this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(1256, 449);
+            this.Controls.Add(this.inventoryTableDataGridView);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtProfitMargin);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtRetailPrice);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtUnitPrice);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtUnitAmt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtItemDesc);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtItemNum);
+            this.Controls.Add(this.label1);
             this.Name = "frmMain";
             this.Text = "Inventory";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.inventoryDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inventoryTableBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inventoryTableDataGridView)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtItemNum;
+        private System.Windows.Forms.TextBox txtItemDesc;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUnitAmt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUnitPrice;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtRetailPrice;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtProfitMargin;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnSubmit;
+        private InventoryDataSet inventoryDataSet;
+        private System.Windows.Forms.BindingSource inventoryTableBindingSource;
+        private InventoryDataSetTableAdapters.InventoryTableTableAdapter inventoryTableTableAdapter;
+        private InventoryDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.DataGridView inventoryTableDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace InventoryDB
 {
     public partial class frmMain : Form
     {
+        SqlConnection con = new SqlConnection("Data Source=PC15\\SQLEXPRESS;Initial Catalog=Inventory;Persist Security Info=True;User ID=Nate;Password=password2");
+
         public frmMain()
         {
             InitializeComponent();
+        }
+
+        private void inventoryTableBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.inventoryTableBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.inventoryDataSet);
+
+        }
+
+        private void inventoryTableBindingNavigatorSaveItem_Click_1(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.inventoryTableBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.inventoryDataSet);
+
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'inventoryDataSet.InventoryTable' table. You can move, or remove it, as needed.
+            this.inventoryTableTableAdapter.Fill(this.inventoryDataSet.InventoryTable);
+
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
